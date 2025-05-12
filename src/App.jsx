d018b3d13cecbf234a665b781509c7c4e1e230a0
Nitheesh Kumar Kambala <nitheeshkumar13350@gmail.com>
2025-05-11 23:46:01 -0400

    react router setup and home page creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
 
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<MainLayout />}>
+      <Route index element={ <HomePage /> } />
+    </Route>
+  )
+);
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-      
-    </>
+    <RouterProvider router={router} />
   )
 }
 
