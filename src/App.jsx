b909e34ce2667f7e22fb7cdc5748a6d88183b9d7
Nitheesh Kumar Kambala <nitheeshkumar13350@gmail.com>
2025-05-13 00:16:34 -0400

    Update job feature and completed all crud operations

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import JobPage, { jobLoader } from './pages/JobPage';
 import MainLayout from './layouts/MainLayout';
 import NotFoundPage from './pages/NotFoundPage';
 import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
   const addJob = async (newJob) => {
@@ -25,6 +26,17 @@ const App = () => {
     return;
   }
 
+  const updateJob = async (updatedJob) => {
+    const res = await fetch(`/api/jobs/${updatedJob.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedJob)
+    });
+    return;
+  } 
+
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element = {<MainLayout />}>
@@ -33,6 +45,7 @@ const App = () => {
         <Route path='/jobs/:id' element={ <JobPage deleteJob={deleteJob} /> } loader={jobLoader} />
         <Route path='/add-job' element={ <AddJobPage addJobSubmit={addJob} /> } />
         <Route path='*' element={ <NotFoundPage /> } />
+        <Route path='/edit-job/:id' element={ <EditJobPage updateJobSubmit={updateJob} /> } loader={jobLoader} />
       </Route>
     )
   );
